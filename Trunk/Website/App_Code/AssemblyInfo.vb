e891805b63451703a0d693a061cccf96cb51d18a
SND\dnnsvn_cp <SND\dnnsvn_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2011-02-21 08:06:25 +0000

    TFS_Update
    git-svn-id: https://dotnetnuke.svn.codeplex.com/svn@58202 142e9c35-602f-4880-9f37-ec48a24daa6a

--- Trunk/Website/App_Code/AssemblyInfo.vb
+++ Trunk/Website/App_Code/AssemblyInfo.vb
@@ -46,7 +46,8 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("5.6.2.79")> 
+<Assembly: AssemblyVersion("5.6.2.80")> 
+
 
 
 
