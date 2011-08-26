715d388faeb5d01169fd9e91be0ba7cd08a46306
SND\dnnsvn_cp <SND\dnnsvn_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2011-08-26 08:22:37 +0000

    TFS_Update
    git-svn-id: https://dotnetnuke.svn.codeplex.com/svn@62778 142e9c35-602f-4880-9f37-ec48a24daa6a

--- Trunk/Modules/Taxonomy/AssemblyInfo.cs
+++ Trunk/Modules/Taxonomy/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")> 
 
-[assembly: AssemblyVersion("6.1.0.239")]
-[assembly: AssemblyFileVersion("6.1.0.239")]
+[assembly: AssemblyVersion("6.1.0.241")]
+[assembly: AssemblyFileVersion("6.1.0.241")]
