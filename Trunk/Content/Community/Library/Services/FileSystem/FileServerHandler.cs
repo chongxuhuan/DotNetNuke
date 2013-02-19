fd88a9532329b72a0a53f20ac37232a0543c3878
SND\dnnsvn_cp <SND\dnnsvn_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2013-02-19 07:31:24 +0000

    Fix bug 24923: call SetThreadCultures function to set the culture correctly.DNN704 Tocs, coverpages, topics, etc.
    git-svn-id: https://dotnetnuke.svn.codeplex.com/svn@71163 142e9c35-602f-4880-9f37-ec48a24daa6a

--- Trunk/Content/Community/Library/Services/FileSystem/FileServerHandler.cs
+++ Trunk/Content/Community/Library/Services/FileSystem/FileServerHandler.cs
@@ -1,7 +1,7 @@
 #region Copyright
 // 
 // DotNetNuke® - http://www.dotnetnuke.com
-// Copyright (c) 2002-2012
+// Copyright (c) 2002-2013
 // by DotNetNuke Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
@@ -95,7 +95,7 @@ namespace DotNetNuke.Services.FileSystem
             }
             if (LocaleController.Instance.IsEnabled(ref Language, _portalSettings.PortalId))
             {
-                Thread.CurrentThread.CurrentUICulture = new CultureInfo(Language);
+                Localization.Localization.SetThreadCultures(new CultureInfo(Language), _portalSettings);
                 Localization.Localization.SetLanguage(Language);
             }
 
