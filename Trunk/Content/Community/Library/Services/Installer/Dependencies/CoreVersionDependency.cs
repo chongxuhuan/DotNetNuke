19344679a2a53e03b98fe8a1147ed4ddcb4eaf79
SND\dnnsvn_cp <SND\dnnsvn_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2013-03-15 17:40:58 +0000

    DNN-25447 - Please adjust text in installer when user tries to upload to a version of DNN that is too old
    git-svn-id: https://dotnetnuke.svn.codeplex.com/svn@71532 142e9c35-602f-4880-9f37-ec48a24daa6a

--- Trunk/Content/Community/Library/Services/Installer/Dependencies/CoreVersionDependency.cs
+++ Trunk/Content/Community/Library/Services/Installer/Dependencies/CoreVersionDependency.cs
@@ -1,7 +1,7 @@
 #region Copyright
 // 
 // DotNetNuke® - http://www.dotnetnuke.com
-// Copyright (c) 2002-2012
+// Copyright (c) 2002-2013
 // by DotNetNuke Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
@@ -47,7 +47,7 @@ namespace DotNetNuke.Services.Installer.Dependencies
         {
             get
             {
-                return Util.INSTALL_Compatibility;
+                return string.Format(Util.INSTALL_Compatibility,minVersion);
             }
         }
 
